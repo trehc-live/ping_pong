e03bbde13fd35c0d177f9721d99d551bfd134007
live_trech <artemalesin89778@gmail.com>
2024-02-03 16:52:16 -0500

    refactoring and some changes

--- ping_pong/test.js
+++ ping_pong/test.js
@@ -1,5 +1,4 @@
 class GameObject{
-    ctx; canvas; x; y; width; height; color; xpluse; ypluse; key_pressed_value; key_pressed; score;
 
     constructor(canvas, ctx, x, y, height, width, color, xpluse, ypluse){
         this.ctx = ctx;
@@ -24,6 +23,13 @@ class GameObject{
     delete(x, y){
         this.ctx.clearRect(x, y, this.width, this.height);
     }
+    
+    ball_respawn(){
+    	this.delete(this.x - this.xpluse, this.y - this.ypluse);
+        this.x = canvas.width / 2;
+        this.y = math.random() * this.canvas.height;
+        this.spawn();
+    }
 }
 
 let canvas = document.getElementById("canvas");
@@ -34,6 +40,19 @@ let player1 = new GameObject(canvas, ctx, 40, 20, 80, 20, 'white', 0, 0);
 let player2 = new GameObject(canvas, ctx, canvas.width - 40, 20, 80, 20, 'white', 0, 0);
 let ball = new GameObject(canvas, ctx, canvas.width / 2, canvas.height / 2, 20, 20, 'white', 4, 1);
 
+function refresh_score(){
+    if(player1.score == 10){
+        alert('left player won!');
+        player1.score = 0; player2.score = 0;
+    }
+
+    if(player2.score == 10){
+        alert('right player won!');
+        player1.score = 0; player2.score = 0;
+    }
+    document.getElementById('score').innerHTML = `${player1.score}:${player2.score}`;
+}
+
 function run_with_walls(){
     ball.delete(ball.x - ball.xpluse, ball.y - ball.ypluse);
     ball.spawn();
@@ -43,41 +62,23 @@ function run_with_walls(){
     if(ball.x <= player1.x + player1.width && ball.x + ball.width >= player1.x && ball.y >= player1.y && ball.y <= player1.y + player1.height){
         ball.xpluse = -ball.xpluse;
     }
-
     if((ball.x >= player2.x + player2.width || ball.x + ball.width >= player2.x) && ball.y >= player2.y && ball.y <= player2.y + player2.height){
         ball.xpluse = -ball.xpluse;
     }
-
     if (ball.x >= canvas.width - ball.height * 2){
-        ball.delete(ball.x - ball.xpluse, ball.y - ball.ypluse);
-        ball.x = canvas.width / 2;
-        ball.y = canvas.height / 2;
-        ball.spawn();
         player1.score++;
-        document.getElementById('score').innerHTML = `${player1.score}:${player2.score}`;
-        if(player1.score == 10){
-            alert('left player won!');
-            player1.score = 0; player2.score = 0;
-        }
-        if(player2.score == 10){
-            alert('right player won!');
-            player1.score = 0; player2.score = 0;
-        }
+        ball.ball_respawn();
+        refresh_score();
     }
-
     if(ball.x <= 0) {
-        ball.delete(ball.x - ball.xpluse, ball.y - ball.ypluse);
-        ball.x = canvas.width / 2;
-        ball.y = canvas.height / 2;
-        ball.spawn();
         player2.score++;
-        document.getElementById('score').innerHTML = `${player1.score}:${player2.score}`;
+        ball.ball_respawn();
+        refresh_score();
     }
-    
     if (ball.y >= canvas.height - ball.height * 2 || ball.y <= 0) {
         ball.ypluse = -ball.ypluse;
     }
-    
+
     ball.y += ball.ypluse;
     ball.x += ball.xpluse;
 }
@@ -111,7 +112,7 @@ function control_key_down_handler(key_value){
         case 'KeyW':{
             if(player1.key_pressed)
                 return;
-            console.log(key_value.code);
+            //console.log(key_value.code);
             player1.key_pressed = true;
             player1.key_pressed_value = 'KeyW'
             action_handler();
@@ -120,7 +121,7 @@ function control_key_down_handler(key_value){
         case 'KeyS':{
             if(player1.key_pressed)
                 return;
-            console.log(key_value.code);
+            //console.log(key_value.code);
             player1.key_pressed = true;
             player1.key_pressed_value = 'KeyS'
             action_handler();
@@ -129,7 +130,7 @@ function control_key_down_handler(key_value){
         case 'KeyI':{
             if(player2.key_pressed)
                 return;
-            console.log(key_value.code);
+            //console.log(key_value.code);
             player2.key_pressed = true;
             player2.key_pressed_value = 'KeyI'
             action_handler();
@@ -138,7 +139,7 @@ function control_key_down_handler(key_value){
         case 'KeyK':{
             if(player2.key_pressed)
                 return;
-            console.log(key_value.code);
+            //console.log(key_value.code);
             player2.key_pressed = true;
             player2.key_pressed_value = 'KeyK'
             action_handler();
@@ -149,34 +150,20 @@ function control_key_down_handler(key_value){
 
 function control_key_up_handler(key_value){
     if(player1.key_pressed && player1.key_pressed_value == key_value.code){
-        console.log(`key up ${key_value.code}`);
+        //console.log(`key up ${key_value.code}`);
         player1.key_pressed = false;
         player1.key_pressed_value = null;
     }
     if(player2.key_pressed && player2.key_pressed_value == key_value.code){
-        console.log(`key up ${key_value.code}`);
+        //console.log(`key up ${key_value.code}`);
         player2.key_pressed = false;
         player2.key_pressed_value = null;
     }
 }
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-}
-
-function pause(key_value){
-    console.log(key_value.code);
-}
-
 function main(){
-    //sleep(10000);
     player1.spawn();
     player2.spawn();
-    window.addEventListener("keypress", pause, false);
     window.addEventListener("keydown", control_key_down_handler, false);
     window.setInterval(action_handler, 10);
     window.addEventListener("keyup", control_key_up_handler, false);
